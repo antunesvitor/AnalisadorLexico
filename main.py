0945957b05ce4119457e47ac4b2fa5e766848f52
Vitor Antunes <vguilhermantunes@gmail.com>
2021-07-23 18:20:11 -0300

    identificando tokens aritméticos

--- main.py
+++ main.py
@@ -11,26 +11,28 @@ def main():
     #PONTUACAO: ;
     #SEPERACAO: espaço em branco, \t ou \n
     #OPERADOR RELACIONAL: > | >= | < | <= | == | !=
+    #OPERADOR ARITMETICO: + | - | * | /
     #ATRIBUIÇÃO: = 
 
     #DFA para saber se uma cadeia de zeros e um's começa com 1 e termina em zero
 
-    alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;\t\n "
+    alfabeto = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;+*-/\t\n "
 
-    relacoes = [('q0', 'abcdefghijklmnopqrstuvwxyz', 'q1'),('q0', '0123456789', 'q2'), ('q0', '=', 'q3'), ('q0', '><', 'q5'), ('q0', '!', 'q6'), ('q0', ' \t\n', 'q7'), ('q0', ';', 'q8'),
-                ('q1', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 'q1'), ('q1', '><!=;\t\n ', 'q0'),
-                ('q2', '0123456789','q2'), ('q2', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ;\t\n><!= ','q0'),
-                ('q3', '=','q4'), ('q3', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!;\t\n ','q0'),
-                ('q4', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;\t\n ', 'q0'),
-                ('q5', '=','q4'), ('q5', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!;\t\n ','q0'),
+    relacoes = [('q0', 'abcdefghijklmnopqrstuvwxyz', 'q1'),('q0', '0123456789', 'q2'), ('q0', '=', 'q3'), ('q0', '><', 'q5'), ('q0', '!', 'q6'), ('q0', ' \t\n', 'q7'), ('q0', ';', 'q8'), ('q0', '+-/*', 'q9'),
+                ('q1', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 'q1'), ('q1', '><!=;+*-/\t\n ', 'q0'),
+                ('q2', '0123456789','q2'), ('q2', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ;\t\n+*-/><!= ','q0'),
+                ('q3', '=','q4'), ('q3', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!;+*-/\t\n ','q0'),
+                ('q4', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;+*-/\t\n ', 'q0'),
+                ('q5', '=','q4'), ('q5', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!;+*-/\t\n ','q0'),
                 ('q6', '=','q4'),
-                ('q7', ' \t\n', 'q7'), ('q7', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;', 'q0'),
-                ('q8', ';', 'q8'), ('q8', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=\t\n ', 'q0')]
+                ('q7', ' \t\n', 'q7'), ('q7', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;+*-/', 'q0'),
+                ('q8', ';', 'q8'), ('q8', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=+*-/\t\n ', 'q0'),
+                ('q9', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789><!=;+*-/\t\n ', 'q0')]
 
     alfabeto_array = [char for char in alfabeto];
 
-    dfa_lexico = AutomatoFinitoDeterministico(['q0', 'q1','q2', 'q3', 'q4', 'q5', 'q6','q7', 'q8'],
-                                                alfabeto_array, relacoes, 'q0',['q1', 'q2', 'q3', 'q4', 'q5', 'q7', 'q8'])
+    dfa_lexico = AutomatoFinitoDeterministico(['q0', 'q1','q2', 'q3', 'q4', 'q5', 'q6','q7', 'q8', 'q9'],
+                                                alfabeto_array, relacoes, 'q0',['q1', 'q2', 'q3', 'q4', 'q5', 'q7', 'q8', 'q9'])
 
 
     #dando nomes aos bois, digo, atribuindo nomes aos estados terminais de nosso AFD (que são os tokens retornados)
@@ -41,11 +43,13 @@ def main():
         'q4': 'RELAÇÃO',
         'q5': 'RELAÇÃO',
         'q7': 'SEPARADOR',
-        'q8': 'PONTUADOR',
+        'q8': 'PONTUADOR', 
+        'q9': 'OP_ARITMÉTICA', 
         None: 'INVÁLIDO'
     }
 
-    cadeias = [ "abc=5;\n3284238 <= 78912312\t "] 
+    # cadeias = [ "a=5;\n3 <= 7\t 3+ asSDF; +-/*pinto"] 
+    cadeias = [ "asSDF; +-/*pinto"] 
     
 
     for cadeia in cadeias:
@@ -55,8 +59,9 @@ def main():
 
         for token in lista_de_tokens:
 
-            if token != 'q7':
-                strg += '{} '.format(nomesTokens[token])
+            # if token != 'q7':
+            #     strg += '{} '.format(nomesTokens[token])
+            strg += '{} '.format(nomesTokens[token])
 
         print(cadeia)
         print(strg)
